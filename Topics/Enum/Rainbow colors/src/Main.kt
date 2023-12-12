2e600c27aa354199260633c7382d05bb0e2d935b
jritxal <javicionax@hotmail.com>
2023-12-12 08:47:45 +0100

    problem solved

---
+++ Topics/Enum/Rainbow colors/src/Main.kt
@@ -0,0 +1,23 @@
+enum class Rainbow(val color:String){
+    RED("red"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    INDIGO("indigo"),
+    VIOLET("violet");
+}
+
+fun belongs(colorToCheck:String):Boolean{
+    for(color in Rainbow.values()){
+        if(color.color == colorToCheck){
+            return true
+        }
+    }
+    return false
+}
+fun main() {
+    // put your code here
+    val color = readln()
+    println(belongs(color))
+}
